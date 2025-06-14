c2f41b4918af0a052ed14a86106383956440ce03
Vlad <laushvl@gmail.com>
2025-06-14 13:56:19 +0400

    Initial commit

---
+++ src/js/__tests__/health.test.js
@@ -0,0 +1,12 @@
+import healthIndicator from '../health.js';
+
+const dataList = [
+    [{name: 'Wizard', health: 90}, 'healthy'],
+    [{name: 'Knight', health: 5}, 'critical'],
+    [{name: 'Thief', health: 40}, 'wounded'],
+]
+
+test.each(dataList)('testing healthIndicator function  for %o', (character, expected) => {
+    const result = healthIndicator(character);
+    expect(result).toBe(expected);
+})
---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+// TODO: write your code here
+import sum from './basic';
+import healthIndicator from './health.js';
+
+console.log('worked');
+
+console.log(sum([1, 2]));
+
+console.log(healthIndicator({name: 'Knight', health: 100}));
